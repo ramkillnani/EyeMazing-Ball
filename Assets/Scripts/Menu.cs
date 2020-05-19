e3a2f33e1a18f3cb0f77dd5d8a9e5aae13b339cd
Peter <pjthompson.au@gmail.com>
2020-05-19 20:47:36 +1000

    MainMenu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+	#region Win Variables
+	[SerializeField] // Shows private in inspector
+	private Text coinText;
+	#endregion
+
+	void Start()
+	{
+		if (SceneManager.GetActiveScene().name == "Win")
+		{
+			// Sets text to the static float coin
+			coinText.text = "Coins collected: " + Coin.coins.ToString();
+		}
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+
+	public void BackToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void RestartGame()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+}
