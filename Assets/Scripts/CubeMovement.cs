d39e9b47cbdcb5efe0370461f015ed358a8b8dd5
ramkillnani <54158723+ramkillnani@users.noreply.github.com>
2020-07-28 17:25:39 +1000

    First Push

---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.localScale = new Vector3(Mathf.Sin(Time.time), 1f, 1f);
+    }
+
+
+}
--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -4,32 +4,77 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-	[SerializeField]
-	Animator trigger;
-	[SerializeField] //visible in the inspector but the variable is still private
-	GameObject door;
+    [SerializeField]
+    Animator trigger;
+    [SerializeField] //visible in the inspector but the variable is still private
+    GameObject door;
 
-	bool isOpened = false; //door is closed by default
+    public Material changeDisolve;
 
-	private void OnTriggerEnter(Collider col)
-	{
-		if (!isOpened) //if the door is not open then-
-		{
-			isOpened = true;
-			door.transform.position += new Vector3(0, 1.392f, 0); //adds 1.392 to the y axis to the objects transform
-			trigger.SetBool("isDown", true);
-			trigger.SetBool("start", false);
-		}
-		else
-		{
-			trigger.SetBool("isDown", true);
-			trigger.SetBool("start", false);
-		}
-	}
+    private float shaderValue = 1f;
+    private float change = 0.01f;
+    private float delay = 0.0f;
 
-	private void OnTriggerExit(Collider colExit)
-	{
-		trigger.SetBool("isDown", false);
-	}
-	   
+    private void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        changeDisolve.SetFloat("Visibility", 1f);
+    }
+
+    IEnumerator ChangeShader()
+    {
+        while (shaderValue > 0f)
+        {
+            yield return new WaitForSeconds(delay);
+
+            if (door.GetComponent<MeshRenderer>().material = changeDisolve)
+            {
+                shaderValue -= change;
+                changeDisolve.SetFloat("Visibility", shaderValue);
+            }
+        }
+
+        if (shaderValue <= 0f)
+        {
+            Destroy(door);
+        }
+
+    }
+
+
+    bool isOpened = false; //door is closed by default
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        StartCoroutine(ChangeShader());
+
+        if (!isOpened) //if the door is not open then-
+        {
+            isOpened = true;
+            //door.transform.position += new Vector3(0, 1.392f, 0); //adds 1.392 to the y axis to the objects transform
+            trigger.SetBool("isDown", true);
+            trigger.SetBool("start", false);
+        }
+        else
+        {
+            trigger.SetBool("isDown", true);
+            trigger.SetBool("start", false);
+        }
+
+        door.GetComponent<Renderer>().material = changeDisolve;
+        
+
+        //Destroy(door, 1.5f);
+    }
+
+    private void OnTriggerExit(Collider colExit)
+    {
+        trigger.SetBool("isDown", false);
+    }
+
 }
